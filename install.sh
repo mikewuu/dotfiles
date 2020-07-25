10c9be35721394fc6e03f312590529174d1abb50
Mike Wu <hello@mikewu.dev>
2020-07-25 23:21:43 +0900

    Added aliases, git configs, and vim files

--- install.sh
+++ install.sh
@@ -1,24 +1,27 @@
 #! /bin/bash
+
 dotfiles=$HOME/.dotfiles
+
 zshrc=$dotfiles/.zshrc
+aliases=$dotfiles/.aliases
 vimrc=$dotfiles/.vimrc
 vimfiles=$dotfiles/.vim
 tmux_conf=$dotfiles/.tmux.conf
+gitignore=$dotfiles/.gitignore
 gitconfig=$dotfiles/.gitconfig
 
-[[ ! -e $HOME/.zshrc ]] && ln -s $zshrc $HOME/.zshrc \
-  || echo ".zshrc already exists..."
+[[ ! -e $HOME/.aliases ]] && ln -s $aliases $HOME/.aliases 
 
-[[ ! -e $HOME/.vimrc ]] && ln -s $vimrc $HOME/.vimrc \
-  || echo ".vimrc already exists..."
+[[ ! -e $HOME/.zshrc ]] && ln -s $zshrc $HOME/.zshrc 
 
-[[ ! -e $HOME/.vim ]] && ln -s $vimfiles $HOME/.vim \
-  || echo ".vim already exists..."
+[[ ! -e $HOME/.vimrc ]] && ln -s $vimrc $HOME/.vimrc 
 
-[[ ! -e $HOME/.tmux.conf ]] && ln -s $tmux_conf $HOME/.tmux.conf \
-  || echo ".tmux.conf already exists..."
+[[ ! -e $HOME/.vim ]] && ln -s $vimfiles $HOME/.vim 
 
-[[ ! -e $HOME/.gitconfig ]] && ln -s $gitconfig $HOME/.gitconfig \
-  || echo ".gitconfig already exists..."
+[[ ! -e $HOME/.tmux.conf ]] && ln -s $tmux_conf $HOME/.tmux.conf 
+
+[[ ! -e $HOME/.gitignore ]] && ln -s $gitignore $HOME/.gitignore 
+
+[[ ! -e $HOME/.gitconfig ]] && ln -s $gitconfig $HOME/.gitconfig 
 
 echo "Done"
