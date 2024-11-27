5d3d52aced1b2589ee910fab8c68f79c0ca791ef
Mike Wu <hello@mikewu.dev>
2024-11-27 17:18:39 +0900

    fix tmux vim nav switch between panes

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,8 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
--- config.enable_tab_bar = false
+config.font_size = 14
 config.window_decorations = "RESIZE"
 -- config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 10
