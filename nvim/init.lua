90fe721731c1f5e33d9ef2316fa080ace1dfee63
Mike Wu <hello@mikewu.dev>
2024-11-30 20:43:34 +0900

    prevent tmux pane nav when zoomed

--- nvim/init.lua
+++ nvim/init.lua
@@ -274,7 +274,18 @@ require('lazy').setup({
   {
     'aserowy/tmux.nvim',
     config = function()
-      require('tmux').setup()
+      require('tmux').setup {
+        navigation = {
+          -- cycles to opposite pane while navigating into the border
+          cycle_navigation = true,
+
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+
+          -- prevents unzoom tmux when navigating beyond vim border
+          persist_zoom = true,
+        },
+      }
     end,
   },
 
