f4c4dea9c9f5da543115e898b9ed3f9dd7594f0f
Mike Wu <hello@mikewu.dev>
2024-12-10 19:50:06 +0900

    remove unused bufferline and gitsigns

--- nvim/init.lua
+++ nvim/init.lua
@@ -1147,7 +1147,6 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.lsp-file-operations',
-  -- require 'plugins.bufferline',
   require 'plugins.starter',
   require 'plugins.avante',
   require 'plugins.folding',
@@ -1155,7 +1154,6 @@ require('lazy').setup({
   require 'plugins.undo-highlight',
   require 'plugins.neotest',
   require 'plugins.ts-comments',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
