14f68f010d866e0bd2d175af959d8ce92b587150
Mike Wu <hello@mikewu.dev>
2025-01-04 05:06:27 +0900

    feat(nvim): add auto-tags

--- nvim/lua/core/opts.lua
+++ nvim/lua/core/opts.lua
@@ -1,3 +1,9 @@
+-- Tabs
+vim.o.tabstop = 2 -- num spaces
+vim.o.expandtab = true -- insert spaces instead of TAB
+vim.o.softtabstop = 2 -- num spaces
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
 vim.filetype.add { pattern = {
   ['.env.*'] = 'sh',
 } }
