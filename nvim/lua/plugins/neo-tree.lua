0b462f8172eb40bbd861d399ce1c1ad31efd2871
Mike Wu <hello@mikewu.dev>
2025-02-27 20:00:06 +0900

    fix(neovim): always auto-size panes on neotree events, and tmux pane events

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -38,5 +38,15 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = "neo_tree_window_after_open",
+        handler = function(_) vim.cmd("wincmd =") end
+      },
+      {
+        event = "neo_tree_window_after_close",
+        handler = function(_) vim.cmd("wincmd =") end
+      },
+    },
   },
 }
