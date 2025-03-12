b3cde13d8ededbcf3d0994e9bfb3c391cd97e3b3
Mike Wu <hello@mikewu.dev>
2025-03-13 00:47:21 +0900

    Revert "Revert "fix(nvim): switch from codeium to copilot""
    This reverts commit 245113e0d07dcc2c7a06053c6f6a3710f022ea56.

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  config = function()
+    require('copilot').setup {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        debounce = 75,
+        keymap = {
+          accept = '<tab>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-n>',
+          prev = '<M-p>',
+          dismiss = '<C-]>',
+        },
+      },
+    }
+  end,
+}
