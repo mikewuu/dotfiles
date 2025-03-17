fa92e5258511af511c248286fe151c97c45fe7a0
Mike Wu <hello@mikewu.dev>
2025-03-17 15:52:16 +0900

    fix(nvim/avante): update default config, increase token count

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,16 +1,16 @@
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
-  lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
+    provider = 'claude',
     claude = {
       endpoint = 'https://api.anthropic.com',
-      model = 'claude-3-5-sonnet-20241022',
+      model = 'claude-3-7-sonnet-20250219',
       timeout = 30000, -- Timeout in milliseconds
       temperature = 0,
-      max_tokens = 4096,
-      disable_tools = true, -- disable tools!
+      max_tokens = 20480,
+      disable_tools = true, -- disable tools!,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -22,8 +22,11 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
       'HakonHarnes/img-clip.nvim',
@@ -33,6 +36,10 @@ return {
         default = {
           embed_image_as_base64 = false,
           prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
           use_absolute_path = true,
         },
       },
