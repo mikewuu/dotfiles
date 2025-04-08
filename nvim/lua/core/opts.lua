be079b8bc920f5c32125affca4230746f7cb3748
Mike Wu <hello@mikewu.dev>
2025-04-08 14:19:52 +0900

    fix(nvim): better equal windows via auto-cmd

--- nvim/lua/core/opts.lua
+++ nvim/lua/core/opts.lua
@@ -4,6 +4,14 @@ vim.o.expandtab = true -- insert spaces instead of TAB
 vim.o.softtabstop = 2 -- num spaces
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
+--equal split
+vim.o.equalalways = false
+vim.api.nvim_create_autocmd({ 'VimResized', 'WinEnter', 'WinLeave' }, {
+  callback = function()
+    vim.cmd 'wincmd ='
+  end,
+})
+
 vim.filetype.add { pattern = {
   ['.env.*'] = 'sh',
 } }
