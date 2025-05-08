6fb67ffcf1cee486e692a521f2d2d98ab04b1a02
Mike Wu <hello@mikewu.dev>
2025-05-08 12:08:04 +0900

    fix(nvim/mason): pin to 1.0.0

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,5 @@
+-- Pin 1.0.0 as there have been breaking changes: https://github.com/mason-org/mason-lspconfig.nvim/issues/545
+return {
+  { 'mason-org/mason.nvim', version = '^1.0.0' },
+  { 'mason-org/mason-lspconfig.nvim', version = '^1.0.0' },
+}
