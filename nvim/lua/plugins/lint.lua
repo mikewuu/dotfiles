aa4a8608d499fc819e711ccd9ae73ec16c22a25c
Mike Wu <hello@mikewu.dev>
2025-05-30 23:37:12 +0900

    fix(neovim): get eslint working in monorepos

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -15,6 +15,21 @@ return {
       linters_by_ft['go'] = { 'golangcilint' }
       linters_by_ft['swift'] = { 'swiftlint' }
 
+      vim.api.nvim_create_user_command('EslintDebug', function()
+        local ft = vim.bo.filetype
+        local current_linters = lint.linters_by_ft[ft] or {}
+        local is_eslint = vim.tbl_contains(current_linters, 'eslint_d')
+
+        print('Current filetype: ' .. ft)
+        print('Linters: ' .. vim.inspect(current_linters))
+
+        if is_eslint then
+          local eslint_config = vim.fn.findfile('eslint.config.js', '.;')
+          print('ESLint config found: ' .. (eslint_config ~= '' and eslint_config or 'None'))
+          print('ESLint args: ' .. vim.inspect(lint.linters.eslint_d.args or 'Default args'))
+        end
+      end, {})
+
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
@@ -57,6 +72,30 @@ return {
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
           if vim.opt_local.modifiable:get() then
+            -- Dynamically update eslint_d args to set the nearest config filetype
+            -- if one is found so that eslint works in monorepo projects
+            local ft = vim.bo.filetype
+            local current_linters = lint.linters_by_ft[ft] or {}
+            local is_eslint = vim.tbl_contains(current_linters, 'eslint_d')
+            if is_eslint then
+              -- Try to find eslint config in project root
+              local eslint_config = vim.fn.findfile('eslint.config.js', '.;')
+
+              if eslint_config ~= '' then
+                lint.linters.eslint_d.args = {
+                  '--config',
+                  eslint_config,
+                  '--format',
+                  'json',
+                  '--stdin',
+                  '--stdin-filename',
+                  function()
+                    return vim.api.nvim_buf_get_name(0) -- current file name
+                  end,
+                }
+              end
+            end
+
             lint.try_lint(nil, {
               -- Ignore errors such as missing eslint configs
               ignore_errors = true,
