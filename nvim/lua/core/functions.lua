bdb98cc4bdbba6188cc272cf5be96f3f9275e7e1
Mike Wu <hello@mikewu.dev>
2025-07-02 16:09:51 +0900

    feat(nvim): add GG command to  grep/glob search

--- nvim/lua/core/functions.lua
+++ nvim/lua/core/functions.lua
@@ -4,3 +4,23 @@
 function P(variable)
   print(vim.inspect(variable))
 end
+
+-- Grep with glob that allows for searching grep only in files that match a certain glob pattern, eg: search inside *.test.ts
+-- Example usage:
+-- :GG *.test.ts
+
+function GrepWithGlob(glob_pattern)
+  local telescope = require 'telescope.builtin'
+  telescope.live_grep {
+    additional_args = function()
+      return { '--glob', glob_pattern }
+    end,
+  }
+end
+
+vim.api.nvim_create_user_command('GG', function(opts)
+  GrepWithGlob(opts.args)
+end, {
+  nargs = 1,
+  complete = 'file', -- optional
+})
