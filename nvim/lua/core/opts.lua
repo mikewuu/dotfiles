1e93a6e6eb04fa34b4b51eb684060a1083b018ac
Mike Wu <hello@mikewu.dev>
2026-03-03 21:43:17 +0900

    Revert "fix(nvim): refactor to separate packages"
    This reverts commit bbd92700b0799538f7646adab24b422ac04a2463.

--- nvim/lua/core/opts.lua
+++ nvim/lua/core/opts.lua
@@ -1,51 +1,20 @@
-vim.g.have_nerd_font = true
+-- Tabs
+vim.o.tabstop = 2 -- num spaces
+vim.o.expandtab = true -- insert spaces instead of TAB
+vim.o.softtabstop = 2 -- num spaces
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Mouse
-vim.opt.mouse = 'a'
-
--- Don't show mode (statusline handles it)
-vim.opt.showmode = false
-
--- Indentation
-vim.o.tabstop = 2
-vim.o.expandtab = true
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.opt.breakindent = true
-
--- Persistent undo
-vim.opt.undofile = true
-
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.inccommand = 'split'
-
--- UI
-vim.opt.signcolumn = 'yes'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.pumheight = 10
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = false
-
--- Timing
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
--- Folding (nvim-ufo)
-vim.o.foldcolumn = '1'
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
--- Keep splits equal size
+--equal split
 vim.o.equalalways = false
+vim.api.nvim_create_autocmd({ 'VimResized', 'WinEnter', 'WinLeave' }, {
+  callback = function()
+    vim.cmd 'wincmd ='
+  end,
+})
 
--- Treat .env.* files as shell
-vim.filetype.add { pattern = { ['.env.*'] = 'sh' } }
+vim.filetype.add { pattern = {
+  ['.env.*'] = 'sh',
+} }
+
+--- Disable characters like eol:↴,tab:»
+vim.opt.list = false
